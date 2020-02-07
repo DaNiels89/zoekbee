edad1b85f52e8e468491e6664d2cd80bec5936b3
DaNiels89 <DaNiels89@github.com>
2020-02-07 15:57:53 -0500

    Added table Posts and post.rb, user.rb has_many :posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_07_202914) do
+ActiveRecord::Schema.define(version: 2020_02_07_205036) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "caption"
+    t.integer "user_id"
+    t.integer "budget"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
